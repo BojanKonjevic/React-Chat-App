5475f2ed968ff54b9c510a1d7d17a7a46c9b82bb
BojanKonjevic <konjevicbojan1@gmail.com>
2024-01-28 20:12:57 +0100

    Login design again

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -15,29 +15,30 @@ export default function Login({ onIdSubmit }) {
 
     return (
         <div className='loginBackground flex h-screen w-screen items-center justify-center flex-col'>
-            <form className='flex items-center justify-center flex-col text-gray-200 bg-gray-800/50 p-10 rounded-2xl shadow-md'>
-                <h1 className='font-serif text-center text-2xl mb-4'>Welcome to Σ Chat! 🗿</h1>
-                <label htmlFor="idInput" className='mb-2 font-serif font-semibold text-xl'>Please Enter Your Id</label>
+            <form className='flex items-center justify-center flex-col text-gray-200 bg-gray-900/50 p-14 rounded-2xl shadow-md'>
+                <h1 className='font-serif text-center text-4xl mb-6'>Welcome to Σ Chat! 🗿</h1>
+                <label htmlFor="idInput" className='mb-4 font-serif font-semibold text-xl'>Please Enter Your Id</label>
                 <div className='flex w-full max-w-md'>
                     <input
                         type="text"
                         id='idInput'
                         ref={idRef}
                         required
-                        className='w-full border-none focus:outline-none p-2 rounded-l bg-gray-200 text-zinc-600'
+                        className='w-full border-none focus:outline-none p-4 text-xl rounded-l bg-gray-200 text-zinc-600'
                     />
                     <button
+                        onClick={handleSubmit}
                         type='submit'
-                        className='bg-blue-500 border-none text-gray-200 font-semibold py-2 px-6 rounded-r hover:bg-blue-600 focus:outline-none cursor-pointer'
+                        className='bg-blue-500 border-none text-lg text-gray-200 font-semibold py-3 px-8 rounded-r hover:bg-blue-600 focus:outline-none cursor-pointer'
                     >
                         Login
                     </button>
                 </div>
-                <div className='mt-4'>
+                <div className='mt-6'>
                     <button
                         onClick={createNewId}
                         type='button'
-                        className='bg-green-500 text-gray-200 font-semibold py-2 px-6 rounded hover:bg-green-600 focus:outline-none cursor-pointer border-none'
+                        className='bg-green-500 text-gray-200 font-semibold py-4 px-9 text-lg rounded hover:bg-green-600 focus:outline-none cursor-pointer border-none'
                     >
                         Create A New ID
                     </button>
